946008a45c9d97958b418b7b0b6113506ca8b038
Joshua D. Miller <josh.miller@outlook.com>
2016-03-15 13:10:36 -0400

    Updated Margarita Auth File to match original Repo

--- margaritaauth.py
+++ margaritaauth.py
@@ -1,46 +1,50 @@
-#!/usr/env python
+#!/usr/bin/python
 '''This python script will launch Jesse Peterson's margarita
 web interface for Reposado.  This modified version however will
 have support for LDAP/AD authentication.  Original code by
 Jesse Peterson https://github.com/jessepeterson/margarita.
 Support for LDAP/AD and PyLinting by Joshua D. Miller
 https://github.com/joshua-d-miller/margarita - josh@psu.edu
-Last Updated July 20, 2015'''
-
+Last Updated March 15, 2016'''
+import getopt
+import os
+import sys
 from flask import Flask
 from flask import jsonify, render_template, redirect
 from flask import request, Response
-# Uncomment the following line to use LDAP
-#from flask.ext.ldap import LDAP
-app = Flask(__name__)
-# Uncomment and fill in the lines below with your AD/LDAP info
-#app.debug = True
-#ldap = LDAP(app)
-#app.config['LDAP_HOST'] = 'servername.com'
-#app.config['LDAP_PORT'] = '636'
-#app.config['LDAP_SCHEMA'] = 'ldaps'
-#app.config['LDAP_DOMAIN'] = 'domain.com'
-#app.config['LDAP_SEARCH_BASE'] = 'OU=YourOUHere,DC=domain,DC=com'
-#app.config['LDAP_REQUIRED_GROUP'] = 'CN=Domain Admins,CN=Users,DC=domain,DC=com'
-#app.secret_key = "makeyourownsecretkey"
-
-import os, sys
+from operator import itemgetter
+from distutils.version import LooseVersion
+from reposadolib import reposadocommon
+# Uncomment the following lines to use LDAP
+# from functools import wraps
+# from flask.ext.ldap import LDAP
 try:
     import json
 except ImportError:
-	# couldn't find json, try simplejson library
+    # couldn't find json, try simplejson library
     import simplejson as json
-import getopt
-from operator import itemgetter
-from distutils.version import LooseVersion
 
-from reposadolib import reposadocommon
-#from functools import wraps
+app = Flask(__name__)
+# Uncomment and fill in the lines below with your AD/LDAP info
+# app.debug = True
+# ldap = LDAP(app)
+# app.config['LDAP_HOST'] = 'servername.com'
+# app.config['LDAP_PORT'] = '636'
+# app.config['LDAP_SCHEMA'] = 'ldaps'
+# app.config['LDAP_DOMAIN'] = 'domain.com'
+# app.config['LDAP_SEARCH_BASE'] = 'OU=YourOUHere,DC=domain,DC=com'
+# app.config['LDAP_REQUIRED_GROUP'] = (
+# 'CN=Domain Admins,CN=Users,DC=domain,DC=com')
+# app.secret_key = "makeyourownsecretkey"
 
-APPLE_CATALOG_VERSION_MAP = {
-    'index-10.11-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog': '10.11',
-    'index-10.10-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog': '10.10',
-    'index-10.9-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog': '10.9',
+
+apple_catalog_version_map = {
+    'index-10.11-10.10-10.9-mountainlion-lion-snowleopard'
+    '-leopard.merged-1.sucatalog': '10.11',
+    'index-10.10-10.9-mountainlion-lion-snowleopard'
+    '-leopard.merged-1.sucatalog': '10.10',
+    'index-10.9-mountainlion-lion-snowleopard'
+    '-leopard.merged-1.sucatalog': '10.9',
     'index-mountainlion-lion-snowleopard-leopard.merged-1.sucatalog': '10.8',
     'index-lion-snowleopard-leopard.merged-1.sucatalog': '10.7',
     'index-leopard-snowleopard.merged-1.sucatalog': '10.6',
@@ -48,46 +52,50 @@ APPLE_CATALOG_VERSION_MAP = {
     'index-1.sucatalog': '10.4',
     'index.sucatalog': '10.4',
 }
+# Uncomment these lines when using LDAP Authentication as well
+# def authenticate():
+    # '''The autnthenication procedure if the user is not authenticated'''
+    # return Response("Couldn't verify your credentials.  Please try again \
+    # or contact your Systems Administrator.", 401, {'WWW-Authenticate': \
+    # 'Basic realm="Login required"'})
 
-#def authenticate():
-    #'''The autnthenication procedure if the user is not authenticated'''
-    #return Response("Couldn't verify your credentials.  Please try again \
-    #or contact your Systems Administrator.", 401, {'WWW-Authenticate': \
-    #'Basic realm="Login required"'})
-
-#def login_required(f):
-	#'''The login_required function will prompt a user for authentication if enabled'''
-    #@wraps(f)
-	#def decorated(*args, **kwargs):
-		#auth = request.authorization
-		#if not auth or not ldap.ldap_login(auth.username, auth.password):
-			#return authenticate()
-		#return f(*args, **kwargs)
-	#return decorated
+# def login_required(f):
+    # '''The login_required function will prompt a user for authentication if enabled'''
+    # @wraps(f)
+        # def decorated(*args, **kwargs):
+            # auth = request.authorization
+            # if not auth or not ldap.ldap_login(auth.username, auth.password):
+                # return authenticate()
+                # return f(*args, **kwargs)
+            # return decorated
 
 # cache the keys of the catalog version map dict
-APPLE_CATALOG_SUFFIXES = APPLE_CATALOG_VERSION_MAP.keys()
+apple_catalog_suffixes = apple_catalog_version_map.keys()
+
 
 def versions_from_catalogs(cats):
     '''Given an iterable of catalogs return the corresponding OS X versions'''
     versions = set()
 
     for cat in cats:
-		# take the last portion of the catalog URL path
+        # take the last portion of the catalog URL path
         short_cat = cat.split('/')[-1]
-        if short_cat in APPLE_CATALOG_SUFFIXES:
-            versions.add(APPLE_CATALOG_VERSION_MAP[short_cat])
+        if short_cat in apple_catalog_suffixes:
+            versions.add(apple_catalog_version_map[short_cat])
 
-    return versions
+        return versions
+
 
 def json_response(r):
     '''Glue for wrapping raw JSON responses'''
     return Response(json.dumps(r), status=200, mimetype='application/json')
 
+
 @app.route('/')
-#@login_required
+# @login_required
 def index():
     return render_template('margarita.html')
+
 
 @app.route('/branches', methods=['GET'])
 def list_branches():
@@ -96,11 +104,12 @@ def list_branches():
 
     return json_response(catalog_branches.keys())
 
+
 def get_description_content(html):
     if len(html) == 0:
         return None
 
-	# in the interest of (attempted) speed, try to avoid regexps
+    # in the interest of (attempted) speed, try to avoid regexps
     lwrhtml = html.lower()
 
     celem = 'p'
@@ -113,11 +122,11 @@ def get_description_content(html):
         celem = 'body'
         startloc = lwrhtml.find('<' + celem)
 
-        if startloc != -1:
-            startloc += 6 # length of <body>
+    if startloc != -1:
+        startloc += 6  # length of <body>
 
     if startloc == -1:
-		# no <p> nor <body> tags. bail.
+        # no <p> nor <body> tags. bail.
         return None
 
     endloc = lwrhtml.rfind('</' + celem + '>')
@@ -125,27 +134,28 @@ def get_description_content(html):
     if endloc == -1:
         endloc = len(html)
     elif celem != 'body':
-		# if the element is a body tag, then don't include it.
-		# DOM parsing will just ignore it anyway
+        # if the element is a body tag, then don't include it.
+        # DOM parsing will just ignore it anyway
         endloc += len(celem) + 3
-
     return html[startloc:endloc]
+
 
 def product_urls(cat_entry):
     '''Retreive package URLs for a given reposado product CatalogEntry.
 
-	Will rewrite URLs to be served from local reposado repo if necessary.'''
+    Will rewrite URLs to be served from local reposado repo if necessary.'''
 
     packages = cat_entry.get('Packages', [])
 
     pkg_urls = []
     for package in packages:
         pkg_urls.append({
-            'url': reposadocommon.rewriteOneURL(package['URL']),
-            'size': package['Size'],
-            })
+                        'url': reposadocommon.rewriteOneURL(package['URL']),
+                        'size': package['Size'],
+                        })
 
     return pkg_urls
+
 
 @app.route('/products', methods=['GET'])
 def products():
@@ -154,35 +164,41 @@ def products():
 
     prodlist = []
     for prodid in products.keys():
-        if 'title' in products[prodid] and 'version' in products[prodid] \
-        and 'PostDate' in products[prodid]:
+        if 'title' in products[prodid] and \
+         'version' in products[prodid] and 'PostDate' in products[prodid]:
             prod = {
-                'title': products[prodid]['title'],
-                'version': products[prodid]['version'],
-                'PostDate': products[prodid]['PostDate'].strftime('%Y-%m-%d'),
-                'description': get_description_content(products[prodid]['description']),
-                'id': prodid,
-                'depr': len(products[prodid].get('AppleCatalogs', [])) < 1,
-                'branches': [],
-                'oscatalogs': sorted(versions_from_catalogs \
-                	(products[prodid].get('OriginalAppleCatalogs')), key=LooseVersion, reverse=True),
-                'packages': product_urls(products[prodid]['CatalogEntry']),
-                }
+             'title': products[prodid]['title'],
+             'version': products[prodid]['version'],
+             'PostDate': products[prodid]['PostDate'].strftime('%Y-%m-%d'),
+             'description': get_description_content
+             (products[prodid]['description']),
+             'id': prodid,
+             'depr': len(products[prodid].get('AppleCatalogs', [])) < 1,
+             'branches': [],
+             'oscatalogs':
+             sorted(versions_from_catalogs
+                    (products[prodid].get('OriginalAppleCatalogs')),
+                    key=LooseVersion, reverse=True),
+             'packages': product_urls(products[prodid]['CatalogEntry']),
+             }
 
-            for branch in catalog_branches.keys():
-                if prodid in catalog_branches[branch]:
-                    prod['branches'].append(branch)
+    for branch in catalog_branches.keys():
+        if prodid in catalog_branches[branch]:
+            prod['branches'].append(branch)
 
             prodlist.append(prod)
         else:
             print 'Invalid update!'
 
-    sprodlist = sorted(prodlist, key=itemgetter('PostDate'), reverse=True)
+            sprodlist = sorted(prodlist,
+                               key=itemgetter('PostDate'), reverse=True)
 
-    return json_response({'products': sprodlist, 'branches': catalog_branches.keys()})
+            return json_response({'products': sprodlist,
+                                  'branches': catalog_branches.keys()})
+
 
 @app.route('/new_branch/<branchname>', methods=['POST'])
-#@login_required
+# @login_required
 def new_branch(branchname):
     catalog_branches = reposadocommon.getCatalogBranches()
     if branchname in catalog_branches:
@@ -190,11 +206,12 @@ def new_branch(branchname):
         abort(401)
     catalog_branches[branchname] = []
     reposadocommon.writeCatalogBranches(catalog_branches)
-    
+
     return jsonify(result='success')
 
+
 @app.route('/delete_branch/<branchname>', methods=['POST'])
-#@login_required
+# @login_required
 def delete_branch(branchname):
     catalog_branches = reposadocommon.getCatalogBranches()
     if not branchname in catalog_branches:
@@ -217,11 +234,12 @@ def delete_branch(branchname):
             os.remove(branchcatalogpath)
 
     reposadocommon.writeCatalogBranches(catalog_branches)
-    
-    return jsonify(result=True);
+
+    return jsonify(result=True)
+
 
 @app.route('/add_all/<branchname>', methods=['POST'])
-#@login_required
+# @login_required
 def add_all(branchname):
     products = reposadocommon.getProductInfo()
     catalog_branches = reposadocommon.getCatalogBranches()
@@ -235,7 +253,7 @@ def add_all(branchname):
 
 
 @app.route('/process_queue', methods=['POST'])
-#@login_required
+# @login_required
 def process_queue():
     catalog_branches = reposadocommon.getCatalogBranches()
 
@@ -248,24 +266,25 @@ def process_queue():
             continue
 
         if change['listed']:
-			# if this change /was/ listed, then unlist it
+            # if this change /was/ listed, then unlist it
             if prodId in catalog_branches[branch]:
                 print 'Removing product %s from branch %s' % (prodId, branch, )
                 catalog_branches[branch].remove(prodId)
         else:
-			# if this change /was not/ listed, then list it
+            # if this change /was not/ listed, then list it
             if prodId not in catalog_branches[branch]:
                 print 'Adding product %s to branch %s' % (prodId, branch, )
                 catalog_branches[branch].append(prodId)
 
-    print 'Writing catalogs'
-    reposadocommon.writeCatalogBranches(catalog_branches)
-    reposadocommon.writeAllBranchCatalogs()
+            print 'Writing catalogs'
+            reposadocommon.writeCatalogBranches(catalog_branches)
+            reposadocommon.writeAllBranchCatalogs()
 
-    return jsonify(result=True)
+            return jsonify(result=True)
+
 
 @app.route('/dup_apple/<branchname>', methods=['POST'])
-#@login_required
+# @login_required
 def dup_apple(branchname):
     catalog_branches = reposadocommon.getCatalogBranches()
 
@@ -273,38 +292,91 @@ def dup_apple(branchname):
         print 'No branch ' + branchname
         return jsonify(result=False)
 
-	# generate list of (non-drepcated) updates
-    products = reposadocommon.getProductInfo()
-    prodlist = []
-    for prodid in products.keys():
-        if len(products[prodid].get('AppleCatalogs', [])) >= 1:
-            prodlist.append(prodid)
+        # generate list of (non-deprecated) updates
+        products = reposadocommon.getProductInfo()
+        prodlist = []
+        for prodid in products.keys():
+            if len(products[prodid].get('AppleCatalogs', [])) >= 1:
+                prodlist.append(prodid)
 
-    catalog_branches[branchname] = prodlist
+        catalog_branches[branchname] = prodlist
 
-    print 'Writing catalogs'
-    reposadocommon.writeCatalogBranches(catalog_branches)
-    reposadocommon.writeAllBranchCatalogs()
+        print 'Writing catalogs'
+        reposadocommon.writeCatalogBranches(catalog_branches)
+        reposadocommon.writeAllBranchCatalogs()
 
-    return jsonify(result=True)
+        return jsonify(result=True)
+
 
 @app.route('/dup/<frombranch>/<tobranch>', methods=['POST'])
-#@login_required
+# @login_required
 def dup(frombranch, tobranch):
     catalog_branches = reposadocommon.getCatalogBranches()
 
-    if frombranch not in catalog_branches.keys() or tobranch not in catalog_branches.keys():
+    if frombranch not in catalog_branches.keys() \
+       or tobranch not in catalog_branches.keys():
         print 'No branch ' + branchname
         return jsonify(result=False)
 
-    catalog_branches[tobranch] = catalog_branches[frombranch]
+        catalog_branches[tobranch] = catalog_branches[frombranch]
 
-    print 'Writing catalogs'
-    reposadocommon.writeCatalogBranches(catalog_branches)
-    reposadocommon.writeAllBranchCatalogs()
+        print 'Writing catalogs'
+        reposadocommon.writeCatalogBranches(catalog_branches)
+        reposadocommon.writeAllBranchCatalogs()
 
-    return jsonify(result=True)
+        return jsonify(result=True)
+
+
+@app.route('/config_data', methods=['POST'])
+def config_data():
+    # catalog_branches = reposadocommon.getCatalogBranches()
+    check_prods = request.json
+
+    if len(check_prods) > 0:
+        cd_prods = \
+         reposadocommon.check_or_remove_config_data_attribute(check_prods)
+    else:
+        cd_prods = []
+
+        response_prods = {}
+        for prod_id in check_prods:
+            response_prods.update(
+                                  {prod_id: True if prod_id in
+                                   cd_prods else False})
+
+            print response_prods
+
+            return json_response(response_prods)
+
+
+@app.route('/remove_config_data/<product>', methods=['POST'])
+def remove_config_data(product):
+    # catalog_branches = reposadocommon.getCatalogBranches()
+    check_prods = request.json
+
+    products = reposadocommon.check_or_remove_config_data_attribute([product, ], remove_attr=True)
+
+    return json_response(products)
+
+
+def main():
+    optlist, args = getopt.getopt(sys.argv[1:], 'db:p:')
+
+    flaskargs = {}
+    flaskargs['host'] = '0.0.0.0'
+    flaskargs['port'] = 8089
+    flaskargs['threaded'] = True
+
+    for o, a in optlist:
+        if o == '-d':
+            flaskargs['debug'] = True
+        elif o == '-b':
+            flaskargs['host'] = a
+        elif o == '-p':
+            flaskargs['port'] = int(a)
+
+        app.run(**flaskargs)
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', debug=True, port=4755, \
-        ssl_context=('YourCertFile', 'YourCertKeyFile'))
+    app.run('0.0.0.0', debug=True, port=4755,
+            ssl_context=('YourCertFile', 'YourCertKeyFile'))
