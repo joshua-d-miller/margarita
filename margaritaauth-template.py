ca2798cb2e4a02934c25014ecaa274b9c0ccfbf7
Joshua D. Miller <josh.miller@outlook.com>
2018-12-04 17:25:49 -0500

    Add 10.13 and 10.14

--- margaritaauth-template.py
+++ margaritaauth-template.py
@@ -41,6 +41,12 @@ app = Flask(__name__)
 
 
 apple_catalog_version_map = {
+    # 10.14 (Snow Leopard)
+    'index-10.14-10.13-10.12-10.11-10.10-10.9-mountainlion-lion'
+    '-snowleopard-leopard.merged-1.sucatalog': '10.14',
+    # 10.13 (High Sierra0)
+    'index-10.13-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard'
+    '-leopard.merged-1.sucatalog': '10.13',
     # 10.12 (Sierra)
     'index-10.12-10.11-10.10-10.9-mountainlion-lion-snowleopard'
     '-leopard.merged-1.sucatalog': '10.12',
